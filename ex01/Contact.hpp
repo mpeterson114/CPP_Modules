3871c4a092045ff914b6174e1d37fcac07eeff00
Megan Peterson <mlpeterson114@Megans-MacBook-Pro.local>
2024-01-19 17:51:59 +0100

    ex01 started

---
+++ ex01/Contact.hpp
@@ -0,0 +1,20 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <iostream>
+
+class Contact {
+private:
+        std::string _firstname;
+        std::string _lastname;
+        std::string _nickname;
+        std::string _number;
+        std::string _secret;
+
+public:
+
+
+
+}
+
+#endif
