a0b52f1b988e9300de6192578b90cdf468363091
Megan Peterson <mlpeterson114@Megans-MacBook-Pro.local>
2024-02-08 14:03:57 +0100

    pre-makefile tests 01/00

---
+++ cpp01/src/Zombie.cpp
@@ -0,0 +1,15 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : name(name)
+{
+};
+
+Zombie::~Zombie()
+{
+    std::cout << "Zombie " << this->name << " was destroyed" << std::endl;
+};
+
+void Zombie::announce(void)
+{
+    std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
