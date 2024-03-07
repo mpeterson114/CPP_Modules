ca3148fb10e833833cd2e2996e65145bfeebecc6
Megan Peterson <mpeterso@c2r1s4.42madrid.com>
2024-03-07 13:10:39 +0100

    pre make tests 05

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,19 @@
 /*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 10:07:31 by mpeterso          #+#    #+#             */
-/*   Updated: 2024/03/07 10:07:32 by mpeterso         ###   ########.fr       */
+/*   Updated: 2024/03/07 13:08:42 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+    
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    return (0);
+}
