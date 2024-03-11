89019675ade3c8e8d13e3982002212eafa86fb82
Megan Peterson <mpeterso@c3r1s5.42madrid.com>
2024-03-11 13:24:40 +0100

    02/02 started

---
+++ cpp02/ex02/inc/Fixed.hpp
@@ -0,0 +1,57 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int _fixedPointVal;
+        static const int _fracBits = 8;
+
+    public:
+        Fixed(void);
+        Fixed(Fixed const &copy);
+        Fixed(int const value);
+        Fixed(float const value);
+        Fixed &operator=(Fixed const &copy);
+         ~Fixed(void);
+
+        /* Comparison operators */
+        bool operator>(Fixed const &value) const;
+        bool operator<(Fixed const &value) const;
+        bool operator>=(Fixed const &value) const;
+        bool operator<=(Fixed const &value) const;
+        bool operator==(Fixed const &value) const;
+        bool operator!=(Fixed const &value) const;
+
+        /* Arithmetic operators */
+        Fixed operator+(Fixed const &value) const;
+        Fixed operator-(Fixed const &value) const;
+        Fixed operator*(Fixed const &value) const;
+        Fixed operator/(Fixed const &value) const;
+
+        /* Increment/Decrement operators */
+        Fixed &operator++(void);  /* pre-increment */
+        Fixed operator++(int);    /* post-increment */
+        Fixed &operator--(void);  /* pre-decrement */
+        Fixed operator--(int);    /* post-decrement */
+
+        /* Static member functions */
+        static Fixed &min(Fixed &x, Fixed &y);
+        static const Fixed &min(Fixed const &x, Fixed const &y);
+        static Fixed &max (Fixed &x, Fixed &y);
+        static const Fixed &max(Fixed const &x, Fixed const &y);
+
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+
+        float toFloat(void) const;
+        int toInt(void) const;
+};
+
+std::ostream  &operator<<(std::ostream &str, Fixed const &fixed_nbr);
+
+
+#endif
---
+++ cpp02/ex02/src/Fixed.cpp
@@ -0,0 +1,159 @@
+#include "Fixed.hpp"
+
+/****************************************************************/
+/*              CONSTRUCTORS & DESTRUCTORS                      */
+/****************************************************************/
+
+Fixed::Fixed(void): _fixedPointVal(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = copy;
+}
+
+Fixed::Fixed(int const value) : _fixedPointVal(value << _fracBits) 
+{
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float const value) : _fixedPointVal(roundf(value * (1 << _fracBits)))
+{
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(Fixed const &copy)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->_fixedPointVal = copy.getRawBits();
+    return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+/****************************************************************/
+/*                   COMPARISON OPERATORS                       */
+/****************************************************************/
+
+bool Fixed::operator>(const Fixed &value) const
+{
+    return this->_fixedPointVal > value._fixedPointVal;
+}
+
+bool Fixed::operator>(Fixed const &value) const
+{
+    return this->_fixedPointVal < value._fixedPointVal;
+}
+
+bool Fixed::operator>=(Fixed const &value) const
+{
+    return this->_fixedPointVal >= value._fixedPointVal;
+}
+
+bool Fixed::operator<=(Fixed const &value) const
+{
+    return this->_fixedPointVal <= value._fixedPointVal;
+}
+
+bool Fixed::operator==(Fixed const &value) const
+{
+    return this->_fixedPointVal == value._fixedPointVal;
+}
+
+bool Fixed::operator!=(Fixed const &value) const
+{
+    return this->_fixedPointVal != value._fixedPointVal;
+}
+
+/****************************************************************/
+/*                   ARITHMETIC OPERATORS                       */
+/****************************************************************/
+
+Fixed Fixed::operator+(Fixed const &value) const
+{
+    return Fixed(this->toFloat() + value.toFloat());
+}
+
+Fixed Fixed::operator-(Fixed const &value) const
+{
+    return Fixed(this->toFloat() - value.toFloat());
+}
+
+Fixed Fixed::operator*(Fixed const &value) const
+{
+    return Fixed(this->toFloat() * value.toFloat());
+}
+
+Fixed Fixed::operator/(Fixed const &value) const
+{
+    return Fixed(this->toFloat() / value.toFloat());
+}
+
+/****************************************************************/
+/*                  INCREMENT/DECREMENT OPERATORS               */
+/****************************************************************/
+
+Fixed &Fixed::operator++(void)              // pre-incrementation
+{
+    ++this->_fixedPointVal;
+    return (*this);
+}
+
+Fixed Fixed::operator++(int)              // post-incrementation
+{
+    Fixed tmp(*this);
+
+    ++(*this);
+    return (tmp);
+}
+
+
+
+
+/****************************************************************/
+/*                  MIN/MAX STATIC FUNCTIONS                    */
+/****************************************************************/
+
+
+
+
+/****************************************************************/
+/*                   GETTERS & SETTERS                          */
+/****************************************************************/
+
+int Fixed::getRawBits(void) const
+{
+    //std::cout << "getRawBits member function called" << std::endl;
+    return (this->_fixedPointVal);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_fixedPointVal = raw;
+}
+
+/****************************************************************/
+/*                OTHER PUBLIC FUNCTIONS                        */
+/****************************************************************/
+
+float Fixed::toFloat(void) const
+{
+    return ((float)this->_fixedPointVal / (1 << _fracBits));
+}
+
+int Fixed::toInt(void) const
+{
+    return this->_fixedPointVal >> _fracBits;
+}
+
+std::ostream  &operator<<(std::ostream &str, Fixed const &fixed_nbr)
+{
+    str << fixed_nbr.toFloat();
+    return (str);
+}
